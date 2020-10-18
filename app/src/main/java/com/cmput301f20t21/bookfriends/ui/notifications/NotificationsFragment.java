5f97a1704664411449fa93bbe5f650aa70f44acd
kdvuong <khangdieuvuong@gmail.com>
2020-10-17 19:41:52 -0600

    fix: rename package

--- app/src/main/java/com/cmput301f20t21/bookfriends/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/cmput301f20t21/bookfriends/ui/notifications/NotificationsFragment.java
@@ -1,4 +1,4 @@
-package com.cmput301f20t21.book_friends.ui.home;
+package com.cmput301f20t21.bookfriends.ui.notifications;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,19 +12,19 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
-import com.cmput301f20t21.book_friends.R;
+import com.cmput301f20t21.bookfriends.R;
 
-public class HomeFragment extends Fragment {
+public class NotificationsFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private NotificationsViewModel notificationsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        notificationsViewModel =
+                ViewModelProviders.of(this).get(NotificationsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        final TextView textView = root.findViewById(R.id.text_notifications);
+        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
