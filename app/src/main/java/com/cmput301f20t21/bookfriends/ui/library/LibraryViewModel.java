41ad63da653bdd7741d38da72d9077a4666bc7ac
c25vdw <lucaszeng99@gmail.com>
2020-10-17 23:54:54 -0600

    chore: create 4 bottom navs and their fragments+controllers

---
+++ app/src/main/java/com/cmput301f20t21/bookfriends/ui/library/LibraryViewModel.java
@@ -0,0 +1,7 @@
+package com.cmput301f20t21.bookfriends.ui.library;
+
+import androidx.lifecycle.ViewModel;
+
+public class LibraryViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
