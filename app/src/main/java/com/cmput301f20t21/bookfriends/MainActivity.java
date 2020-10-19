c1ef15c34109e6f5fa368013c087688ac6fb2af4
TRUNG TRAN <quoctrun@ualberta.ca>
2020-10-19 16:41:44 -0600

    Removed unnecessary code

--- app/src/main/java/com/cmput301f20t21/bookfriends/MainActivity.java
+++ app/src/main/java/com/cmput301f20t21/bookfriends/MainActivity.java
@@ -31,10 +31,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-
-        LibraryFragment libraryFragment = new LibraryFragment();
-        FragmentManager fm = getSupportFragmentManager();
-        fm.beginTransaction().add(R.id.library_fragment, libraryFragment);
     }
 
 }
