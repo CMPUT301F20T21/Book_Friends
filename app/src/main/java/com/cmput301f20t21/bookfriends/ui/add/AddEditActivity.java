e4934ba0da6256bc55c9ad7f84f6fa8f15f881fe
TRUNG TRAN <quoctrun@ualberta.ca>
2020-10-20 23:28:04 -0600

    Upload-image feature working like a charm

--- app/src/main/java/com/cmput301f20t21/bookfriends/ui/add/AddEditActivity.java
+++ app/src/main/java/com/cmput301f20t21/bookfriends/ui/add/AddEditActivity.java
@@ -1,12 +1,17 @@
 package com.cmput301f20t21.bookfriends.ui.add;
 
+import android.Manifest;
 import android.app.ActionBar;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 
 import androidx.annotation.NonNull;
@@ -24,6 +29,10 @@ public class AddEditActivity extends AppCompatActivity {
     private TextInputLayout titleEditText;
     private TextInputLayout authorEditText;
     private TextInputLayout descriptionEditText;
+
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_CODE = 1001;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,7 +57,19 @@ public class AddEditActivity extends AppCompatActivity {
         uploadImgButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                uploadImg();
+                // check permission
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        == PackageManager.PERMISSION_DENIED) { // permission not granted, ask for it
+                        String[] permission = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                        // pop up to request permission
+                        requestPermissions(permission, PERMISSION_CODE);
+                    } else { // permission granted
+                        uploadImg();
+                    }
+                } else { // if android version is less than marshmallow 6.0.1
+                    uploadImg();
+                }
             }
         });
 
@@ -108,5 +129,44 @@ public class AddEditActivity extends AppCompatActivity {
 
     public void uploadImg() {
         // TODO: implement the feature that allows user to upload cover image
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_CODE);
+    }
+
+    /**
+     * handle result of runtime permission
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    uploadImg();
+                } else {
+                    // permission denied by user
+                    Toast.makeText(this, "Permission denied...!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }
+    }
+
+    /**
+     * set the cover image after selecting an image from gallery
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            // set image to image view
+            bookImage.setImageURI(data.getData());
+        }
     }
 }
