99da187830cca2684f5bcf826058492abddc531f
kdvuong <khangdieuvuong@gmail.com>
2020-10-28 17:09:59 -0600

    fix: add 8dp to button bottom margin

--- app/src/main/java/com/cmput301f20t21/bookfriends/ui/library/OwnedViewModel.java
+++ app/src/main/java/com/cmput301f20t21/bookfriends/ui/library/OwnedViewModel.java
@@ -9,14 +9,7 @@ import java.util.ArrayList;
 public class OwnedViewModel extends ViewModel {
     public ArrayList<Book> getBooks() {
         ArrayList<Book> mData = new ArrayList<>();
-        mData.add(new Book ("The two towers" ));
-        mData.add(new Book ("The Lord of the Rings"));
-        mData.add(new Book ("Notes for CMPUT301"));
-        mData.add(new Book ("The Return of the King"));
-        mData.add(new Book ("The Hobbit"));
-        mData.add(new Book ("The Return of the King"));
-        mData.add(new Book ("The Return of the King"));
-        mData.add(new Book ("The Return of theKing"));
+
         return mData;
     }
 }
