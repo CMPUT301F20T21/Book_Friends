75a0499745d17ea9065664b005a2c56ab4b91a9d
zhpeng811 <zhpeng@ualberta.ca>
2020-11-01 20:36:25 -0700

    refactor & doc: refactor validation to viewModel and add javadoc for LoginActivity and LoginViewModel

--- app/src/main/java/com/cmput301f20t21/bookfriends/ui/login/LoginActivity.java
+++ app/src/main/java/com/cmput301f20t21/bookfriends/ui/login/LoginActivity.java
@@ -15,12 +15,14 @@ import android.os.Handler;
 import android.text.Editable;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.cmput301f20t21.bookfriends.MainActivity;
 import com.cmput301f20t21.bookfriends.R;
+import com.cmput301f20t21.bookfriends.enums.LOGIN_ERROR;
 import com.cmput301f20t21.bookfriends.ui.component.ProgressButton;
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -35,11 +37,11 @@ public class LoginActivity extends AppCompatActivity {
     private View loginButton;
     private ProgressButton progressButton;
     private LoginViewModel model;
+
     /**
      * android lifecycle method, grab the Layout and EditText fields
      * as well as adding a simple textChangedListener
-     *
-     * @param savedInstanceState - the saved objects, should contain nothing for this activity
+     * @param savedInstanceState the saved objects, should contain nothing for this activity
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,72 +81,45 @@ public class LoginActivity extends AppCompatActivity {
                 this, loginButton,
                 getString(R.string.string_login),
                 getString(R.string.string_logged_in));
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onLoginClick(v);
-            }
-        });
+        loginButton.setOnClickListener(this::onLoginClick);
     }
 
     /**
-     * check the username field for emptiness, adding error message if field is empty
-     *
-     * @param username - the username that the user entered
-     * @return true if the username is not empty, false if username is empty
-     */
-    private boolean checkUsername(String username) {
-        boolean isUsernameEmpty = username.isEmpty();
-        if (isUsernameEmpty) {
-            usernameLayout.setError(getString(R.string.username_empty_error));
-        }
-        return !isUsernameEmpty;
-    }
-
-    /**
-     * check the password field for emptiness, adding error message if field is empty
-     *
-     * @param password - the password that the user entered
-     * @return true if the password is not empty, false if password is empty
-     */
-    private boolean checkPassword(String password) {
-        boolean isPasswordEmpty = password.isEmpty();
-        if (isPasswordEmpty) {
-            passwordLayout.setError(getString(R.string.password_empty_error));
-        }
-        return !isPasswordEmpty;
-    }
-
-    /**
-     * TODO: fill out javadoc comments once finished
-     *
-     * @param view
+     * method called when the "Create Account" button is clicked
+     * user will be redirected to an new activity to create an account
+     * @param view the view associated with the button
      */
     public void onCreateAccountClicked(View view) {
         startActivity(new Intent(this, CreateAccountActivity.class));
-        // start the activity here, no result is needed
-        // created user will be stored directly to firestore
     }
 
     /**
      * method called when user clicked the "login" button
      * the function will validate the inputs and authenticate the user
      * user will be redirected to the main screen if authentication is successful
-     *
-     * @param view - the view associated with the button
+     * @param view the view associated with the button
      */
     public void onLoginClick(View view) {
         String username = usernameField.getText().toString();
         String password = passwordField.getText().toString();
-        if (checkUsername(username) && checkPassword(password)) {
+        boolean isInputValid = true;
+        if (model.isEmpty(usernameLayout)) {
+            usernameLayout.setError(getString(R.string.username_empty_error));
+            isInputValid = false;
+        }
+        if (model.isEmpty(passwordLayout)) {
+            passwordLayout.setError(getString(R.string.password_empty_error));
+            isInputValid = false;
+        }
+        if (isInputValid) {
             progressButton.onClick();
-            model.handleLogIn(username, password,
-                    () -> onLoginSuccess(),
-                    () -> onLoginFail()
-            );
+            model.handleLogIn(username, password, this::onLoginSuccess, this::onLoginFail);
         }
     }
 
+    /**
+     * callback function called when user successfully login
+     */
     private void onLoginSuccess() {
         progressButton.onSuccess();
         Handler handler = new Handler();
@@ -155,9 +130,22 @@ public class LoginActivity extends AppCompatActivity {
         }, 500);
     }
 
-    private void onLoginFail() {
+    /**
+     * callback function called when user failed the authentication process
+     */
+    private void onLoginFail(LOGIN_ERROR error) {
         progressButton.onError();
-        usernameLayout.setError("Error"); // on failure
+        String errorMessage;
+        switch (error) {
+            case CANNOT_FIND_USERNAME:
+                usernameLayout.setError(getString(R.string.login_username_error));
+                break;
+            case INCORRECT_PASSWORD:
+                passwordLayout.setError(getString(R.string.login_password_error));
+                break;
+            default:
+                Toast.makeText(this, getString(R.string.unexpected_error), Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
