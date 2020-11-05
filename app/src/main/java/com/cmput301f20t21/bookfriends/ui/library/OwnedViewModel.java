d06e8313069da683884be5344dda33a92dc6574b
zhpeng811 <zhpeng@ualberta.ca>
2020-11-04 22:41:03 -0700

    doc: add javadoc for all ViewModel classes

--- app/src/main/java/com/cmput301f20t21/bookfriends/ui/library/OwnedViewModel.java
+++ app/src/main/java/com/cmput301f20t21/bookfriends/ui/library/OwnedViewModel.java
@@ -1,3 +1,13 @@
+/*
+ * OwnedViewModel.java
+ * Version: 1.0
+ * Date: November 4, 2020
+ * Copyright (c) 2020. Book Friends Team
+ * All rights reserved.
+ * github URL: https://github.com/CMPUT301F20T21/Book_Friends
+ */
+
+
 package com.cmput301f20t21.bookfriends.ui.library;
 
 
@@ -17,6 +27,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+/**
+ * The ViewModel for OwnedListFragment
+ */
 public class OwnedViewModel extends ViewModel {
 
     private final IAuthRepository authRepository = AuthRepository.getInstance();
@@ -32,27 +45,53 @@ public class OwnedViewModel extends ViewModel {
         fetchBooks();
     }
 
+    /**
+     * get a list of books
+     * @return a list of book
+     */
     public LiveData<List<Book>> getBooks() {
         return books;
     }
 
+    /**
+     * get the updated position
+     * @return the updated position
+     */
     public LiveData<Integer> getUpdatedPosition() {
         return updatedPosition;
     }
 
+    /**
+     * get the error message observer
+     * @return the error message observer
+     */
     public LiveData<BOOK_ERROR> getErrorMessageObserver() {
         return errorMessageObserver;
     }
 
+    /**
+     * get a book object by list index
+     * @param index the index of the book to get
+     * @return the book at the specified index
+     */
     public Book getBookByIndex(Integer index) {
         return bookData.get(index);
     }
 
+    /**
+     * add a book to the list
+     * @param book the book to add
+     */
     public void addBook(Book book) {
         bookData.add(book);
         books.setValue(bookData);
     }
 
+    /**
+     * update a book that is in the list
+     * @param oldBook the old book to be updated
+     * @param updatedBook the new book to update
+     */
     public void updateBook(Book oldBook, Book updatedBook) {
         Integer indexToUpdate = bookData.indexOf(oldBook);
         if (indexToUpdate != -1) {
@@ -63,6 +102,10 @@ public class OwnedViewModel extends ViewModel {
         }
     }
 
+    /**
+     * delete a book locally and from firestore
+     * @param book the book to delete
+     */
     public void deleteBook(Book book) {
         bookRepository.delete(book.getId())
                 .addOnSuccessListener(
@@ -74,6 +117,9 @@ public class OwnedViewModel extends ViewModel {
                 .addOnFailureListener(e -> errorMessageObserver.setValue(BOOK_ERROR.FAIL_TO_DELETE_BOOK));
     }
 
+    /**
+     * fetch a list of book that belongs to current user
+     */
     private void fetchBooks() {
         String currentUsername = authRepository.getCurrentUser().getUsername();
         bookRepository.getBooksOfOwnerId(currentUsername)
