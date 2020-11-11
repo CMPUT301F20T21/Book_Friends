df88ca374069648c12a420827b21f21bf3464192
Meilin-Lyu <mlyu@ualberta.ca>
2020-11-10 20:23:00 -0700

    delete unit test

--- app/src/test/java/com/cmput301f20t21/bookfriends/viewmodels/CreateAccountViewModelUnitTest.java
+++ app/src/test/java/com/cmput301f20t21/bookfriends/viewmodels/CreateAccountViewModelUnitTest.java
@@ -2,15 +2,12 @@ package com.cmput301f20t21.bookfriends.viewmodels;
 
 import com.cmput301f20t21.bookfriends.entities.User;
 import com.cmput301f20t21.bookfriends.enums.SIGNUP_ERROR;
-import com.cmput301f20t21.bookfriends.exceptions.UnexpectedException;
 import com.cmput301f20t21.bookfriends.fakes.callbacks.FakeFailCallbackWithMessage;
 import com.cmput301f20t21.bookfriends.fakes.callbacks.FakeSuccessCallback;
 import com.cmput301f20t21.bookfriends.fakes.repositories.FakeAuthRepository;
 import com.cmput301f20t21.bookfriends.fakes.repositories.FakeUserRepository;
-import com.cmput301f20t21.bookfriends.fakes.tasks.FakeFailTask;
 import com.cmput301f20t21.bookfriends.fakes.tasks.FakeSuccessTask;
 import com.cmput301f20t21.bookfriends.ui.login.CreateAccountViewModel;
-import com.google.firebase.auth.AuthResult;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -56,81 +53,4 @@ public class CreateAccountViewModelUnitTest {
 
     }
 
-    @Test
-    public void signUpFail_unexpectedError() {
-        CreateAccountViewModel model = new CreateAccountViewModel(mockAuthRepository, mockUserRepository);
-
-        FakeFailTask fakeUserTask = new FakeFailTask(new UnexpectedException());
-        FakeSuccessTask<AuthResult> fakeAuthResultTask = new FakeSuccessTask(null);
-        when(mockUserRepository.getByUsername(username)).thenReturn(fakeUserTask);
-        when(mockAuthRepository.signIn(username, email, password)).thenReturn(fakeAuthResultTask);
-
-
-        model.handleSignUp(username, email,password, mockSuccessCallback, mockFailCallbackWithMessage);
-
-        verify(mockFailCallbackWithMessage, times(1)).run(SIGNUP_ERROR.UNEXPECTED);
-    }
-
-
-    @Test
-    public void signUpFail_EMAIL() {
-        CreateAccountViewModel model = new CreateAccountViewModel(mockAuthRepository, mockUserRepository);
-
-        FakeFailTask fakeUserTask = new FakeFailTask(new UnexpectedException());
-
-        when(mockUserRepository.getByUsername(username)).thenReturn(fakeUserTask);
-
-        model.handleSignUp(username, email,password, mockSuccessCallback, mockFailCallbackWithMessage);
-
-        verify(mockFailCallbackWithMessage, times(1)).run(SIGNUP_ERROR.UNEXPECTED);
-    }
-
-
-
 }
-
-
-//    @Test
-//    public void signUpFail() {
-//        LoViewModel model = new LoginViewModel(mockAuthRepository, mockUserRepository);
-//
-//        FakeFailTask fakeUserTask = new FakeFailTask(new UsernameNotExistException());
-//
-//        when(mockUserRepository.getByUsername(username)).thenReturn(fakeUserTask);
-//
-//        model.handleLogIn(username, password, mockSuccessCallback, mockFailCallbackWithMessage);
-//
-//        verify(mockFailCallbackWithMessage, times(1)).run(LOGIN_ERROR.CANNOT_FIND_USERNAME);
-//    }
-
-//    @Test
-//    public void loginFail_unexpectedError() {
-//        LoginViewModel model = new LoginViewModel(mockAuthRepository, mockUserRepository);
-//
-//        FakeFailTask fakeUserTask = new FakeFailTask(new UnexpectedException());
-//
-//        when(mockUserRepository.getByUsername(username)).thenReturn(fakeUserTask);
-//
-//        model.handleLogIn(username, password, mockSuccessCallback, mockFailCallbackWithMessage);
-//
-//        verify(mockFailCallbackWithMessage, times(1)).run(LOGIN_ERROR.UNEXPECTED);
-//    }
-//
-//    @Test
-//    public void loginFail_incorrectPassword() {
-//        LoginViewModel model = new LoginViewModel(mockAuthRepository, mockUserRepository);
-//        User user = new User(uid, username, email);
-//
-//        FakeSuccessTask<User> fakeUserTask = new FakeSuccessTask(user);
-//        FakeFailTask fakeAuthResultTask = new FakeFailTask(new InvalidLoginCredentialsException());
-//
-//        when(mockUserRepository.getByUsername(username)).thenReturn(fakeUserTask);
-//        when(mockAuthRepository.signIn(username, email, password)).thenReturn(fakeAuthResultTask);
-//
-//        model.handleLogIn(username, password, mockSuccessCallback, mockFailCallbackWithMessage);
-//
-//        verify(mockFailCallbackWithMessage, times(1)).run(LOGIN_ERROR.INCORRECT_PASSWORD);
-//    }
-//}
-//
-//}
