216f4ad41c51c389bc38a0e6025226bff34023e3
c25vdw <lucaszeng99@gmail.com>
2020-11-18 00:26:24 -0700

    chore: import firebase cloud messaging

---
+++ app/src/main/java/com/cmput301f20t21/bookfriends/services/BookFriendsFirebaseMessagingService.java
@@ -0,0 +1,34 @@
+package com.cmput301f20t21.bookfriends.services;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class BookFriendsFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = "bfriends_messaging";
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+        Log.e(TAG, "Refreshed token: " + s);
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+    }
+}
